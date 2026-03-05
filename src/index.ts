a294eb569e7d128e53f2c772699a7cfce5a87588
abeshumpei <harrier2070@gmail.com>
2026-03-05 23:06:15 +0900

    apiの修正

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { neon } from "@neondatabase/serverless";
 
 const app = new Hono();
 
@@ -7,8 +8,15 @@ const welcomeStrings = [
   "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
 ];
 
+const sql = neon(process.env.DATABASE_URL!);
+
 app.get("/", (c) => {
   return c.text(welcomeStrings.join("\n\n"));
+});
+
+app.get("/users", async (c) => {
+  const users = await sql`SELECT * FROM users`;
+  return c.json(users);
 });
 
 export default app;
