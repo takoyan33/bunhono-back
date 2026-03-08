414ef4bd3c1b8f9beb51698044031ae489d2c0a4
abeshumpei <harrier2070@gmail.com>
2026-03-08 22:41:06 +0900

    index.tsをimportに修正

--- api/app.ts
+++ api/app.ts
@@ -5,8 +5,13 @@ export const app = new Hono();
 
 const sql = neon(process.env.DATABASE_URL!);
 
+const welcomeStrings = [
+  "Hello Hono!",
+  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
+];
+
 app.get("/", (c) => {
-  return c.json({ message: "Hello Hono 🚀" });
+  return c.text(welcomeStrings.join("\n\n"));
 });
 
 app.get("/users", async (c) => {
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import { Hono } from "hono";
-import { neon } from "@neondatabase/serverless";
-
-const app = new Hono();
-
-const welcomeStrings = [
-  "Hello Hono!",
-  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
-];
-
-const sql = neon(process.env.DATABASE_URL!);
-
-app.get("/", (c) => {
-  return c.text(welcomeStrings.join("\n\n"));
-});
-
-app.get("/users", async (c) => {
-  const users = await sql`SELECT * FROM users`;
-  return c.json(users);
-});
+import { app } from "../api/app";
 
 export default app;
